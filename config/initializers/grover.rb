ce1f3acef7e131776a53a4d56ce6624af0a9a4aa
shannon fuit <shannon.fuit@gmail.com>
2025-09-27 17:49:26 +0200

    prepare app for deploy

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,3 +1,6 @@
 Grover.configure do |config|
-  config.options = { launch_args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+  config.options = {
+    launch_args: [ "--no-sandbox", "--disable-setuid-sandbox" ],
+    executable_path: ENV.fetch("GOOGLE_CHROME_BIN", nil)
+  }
 end
