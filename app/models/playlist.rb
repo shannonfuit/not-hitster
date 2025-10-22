0f9165a3546696c038dda3041c337a15dbb98771
shannon fuit <shannon.fuit@gmail.com>
2025-10-22 12:01:51 +0200

    add icon for extension pack

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,10 @@ class Playlist < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :spotify_url, presence: true
+
+  BARTS_HITS_TITLE = "Bart's Hits"
+
+  def barts_hits?
+    name == BARTS_HITS_TITLE
+  end
 end
